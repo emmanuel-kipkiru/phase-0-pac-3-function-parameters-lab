3c2808accbbbed7e7964997152bd46c7ad298d26
emmanuel-kipkiru <emmanuelkipkirui17@gmail.com>
2022-06-05 17:39:25 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+function introduction (name) {
+return 'Hi, my name is Aki';
+}
+function introductionWithLanguage(name, language) {
+    return 'Hi, my name is ${Aki} and I am learning to program in ${language}.'
+} 
+function introductionWithLanguageOptional(name, language) {
+    return 'hi, my name is ${Aki} and i am learning to program in ${language}.'
+}
